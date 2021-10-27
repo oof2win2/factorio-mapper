fb0ce8f833985ac2c3cc29aa6dfb92492e2ed240
oof2win2 <honza.koco44@gmail.com>
2021-10-27 11:51:44 +0200

    :horse: a bit of a performance boost?

--- factorio-mapper-mod/control.lua
+++ factorio-mapper-mod/control.lua
@@ -1,25 +1,55 @@
 local function screenshot (opts)
-	local output = {}
+	local tiles = {}
+	local entities = {}
+	local tileColors = {}
+	local entityColors = {}
 	local surface = opts and opts.player_index and game.get_player(opts.player_index).surface or game.get_surface(1)
+	local n = 0
 	for chunk in surface.get_chunks() do
+		n = n + 1
 		if surface.is_chunk_generated({chunk.x, chunk.y}) then
-			for y = chunk.area.left_top.y, chunk.area.right_bottom.y do
-				for x = chunk.area.left_top.x, chunk.area.right_bottom.x do
-					local tile = surface.get_tile(x, y)
-					local entity = surface.find_entities({left_top={x+0.5, y+0.5}, right_bottom={x+0.5, y+0.5}})[1]
-					table.insert(output, {
-						x=x+0.5, y=y+0.5,
-						-- tilename=tile and tile.name or "none",
-						tilecolor=tile and tile.prototype and tile.prototype.map_color or "none",
-						-- entityname=entity and entity.name or "none"
-						entitycolor=entity  and entity.prototype and entity.prototype.map_color or "none"
-					})
+			local chunkBoundingBox={left_top=chunk.area.left_top, right_bottom=chunk.area.right_bottom}
+			log(serpent.line(chunkBoundingBox))
+			local prof = game.create_profiler(false)
+			local foundEntities = surface.find_entities(chunkBoundingBox)
+			local foundTiles = surface.find_tiles_filtered{
+				area=chunkBoundingBox
+			}
+			log(prof)
+
+
+			prof.reset()
+			-- for _, entity in pairs(foundEntities) do
+			-- 	table.insert(entities, {
+			-- 		x=entity.position.x+0.5, y=entity.position.y+0.5,
+			-- 		color=entity.prototype and entity.prototype.map_color or "none"
+			-- 	})
+			-- end
+
+			-- local i = 0
+			for i, tile in pairs(foundTiles) do
+				if tile.position.x % 4 == 0 and tile.position.y % 4 == 0 then
+					-- local tileColor = tileColors[tile.name]
+					-- if tileColor == nil then 
+					-- 	tileColor = tile.prototype.map_color or "none"
+					-- 	tileColors[tile.name] = tileColor
+					-- end
+					foundTiles[i] = {
+						pos=tile.position,
+						color=tile.prototype.map_color
+					}
+					-- i = i+1
 				end
 			end
+			log(prof)
 		end
 	end
+	log(tostring(n) .. " chunks have been found")
 	-- return rcon.print(game.table_to_json(output))
-	game.write_file("headless-screenshot.out", game.table_to_json(output), false)
+	game.write_file("headless-screenshot.out", game.table_to_json({
+		tiles=tiles,
+		entities=entities,
+	}), false)
 end
 
 commands.add_command("headless-screenshot", nil, screenshot)
